b8f8ba092394e969e0038166bf3e0ef942cbd2e1
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2024-02-13 19:16:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/exercise' element={<Exercises/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 function NavBar(){
 
@@ -10,10 +10,7 @@ function NavBar(){
 
 
     return(
-        <>
-        
-        
-        </>
+       <Link to="/">Home</Link>
     )
 }
 
